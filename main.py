28d4ab6d3efbcd50a8992318298eb3db4191dfdf
Somesh BR <somuresearch1@gmail.com>
2023-12-24 12:43:51 +0530

    Day 22: Render the app titles in two columns

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 
@@ -20,4 +21,16 @@ content2 = """Below you can find some of the apps and
         I have built in Python! """
 
 st.write(content2)
+
+df = pandas.read_csv("data.csv", sep=";") # This function used to read the csv file in which can access data in rows
+
+col3,col4 = st.columns(2)
+
+with col3:  # This is used to display the header for 1st 10 items from Excel.
+    for index, row in df[:10].iterrows(): # Iterrows is method which gives access to rows which can iterate over the rows.
+        st.header(row['title'])
+
+with col4: # This is used to display the header for next  10 items from  Excel.
+    for index, row in df[:10].iterrows():
+        st.header(row['title'])
 
