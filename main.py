7cee735e88f3aef3120797aa8f6019d502722442
Somesh BR <somuresearch1@gmail.com>
2023-12-24 11:57:13 +0530

    Day 21: Add Description column

--- main.py
+++ main.py
@@ -12,7 +12,12 @@ with col2:
     st.title("Somesh B R")
     content = """
     I am a Python Programmer whose interested in solving  different problems which occurs in day to day life.
-    Trying to find solution in the form of code.
+    Trying to find solution in the form of code I have completed my Engineering in Cambridge Institute of Technology.
     """
     st.info(content)
+
+content2 = """Below you can find some of the apps and 
+        I have built in Python! """
+
+st.write(content2)
 
